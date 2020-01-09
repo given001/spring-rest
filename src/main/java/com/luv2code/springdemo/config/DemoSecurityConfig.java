2669194481af6b9b267ceaedbc7bd34de2e1d887
Nguyen Thang <thangit96@gmail.com>
2020-01-09 20:32:32 +0700

    First commit

---
+++ src/main/java/com/luv2code/springdemo/config/DemoSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.luv2code.springdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// add user in memory authentication
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+				.withUser(users.username("thang").password("test123").roles("EMPLOYEE"))
+				.withUser(users.username("mary").password("test123").roles("EMPLOYEE","MANAGER"))
+				.withUser(users.username("john").password("test123").roles("EMPLOYEE","ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// sercure all REST endpoints
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "api/customers/").hasRole("EMPLOYEE")
+			.antMatchers(HttpMethod.GET, "api/customers/**").hasRole("EMPLOYEE")
+			.antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers(HttpMethod.POST, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers(HttpMethod.PUT, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers(HttpMethod.DELETE, "/api/customers/**").hasRole("ADMIN")
+			.and()
+			.httpBasic()
+			.and()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	
+}
